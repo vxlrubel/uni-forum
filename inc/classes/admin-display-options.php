c7e4816c8f75695edfa5e1c7d3b816bd69464859
rubel <vxlrubel@gmail.com>
2024-03-29 21:47:17 +0600

    create a file for display settings options.

---
+++ inc/classes/admin-display-options.php
@@ -0,0 +1,17 @@
+<?php
+
+// directly access denied
+defined('ABSPATH') || exit;
+?>
+
+<div class="wrap uf-reset">
+    <h1 class="uni-settings-title">Uni Forum Settings</h1>
+    <div class="uf-settings-parent">
+        <div class="options-settings">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate repudiandae autem mollitia ipsam sapiente ipsum numquam. Hic maiores, nam eius dolorum odit, ea eveniet possimus porro omnis officia doloremque! Libero?
+        </div>
+        <div class="options-author">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, voluptas explicabo! Ab enim voluptatibus reiciendis, totam beatae, facilis est iusto accusantium sapiente eligendi earum maxime sint praesentium consectetur dolorem impedit.
+        </div>
+    </div>
+</div>
