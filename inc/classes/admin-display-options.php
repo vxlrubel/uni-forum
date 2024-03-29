19f0c0537fbd472ce273f5e9f0f667069b2702b4
rubel <vxlrubel@gmail.com>
2024-03-29 22:27:03 +0600

    update settings page optios.

--- inc/classes/admin-display-options.php
+++ inc/classes/admin-display-options.php
@@ -6,12 +6,39 @@ defined('ABSPATH') || exit;
 
 <div class="wrap uf-reset">
     <h1 class="uni-settings-title">Uni Forum Settings</h1>
-    <div class="uf-settings-parent">
+    <div class="uf-settings-parent border p-20">
         <div class="options-settings">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate repudiandae autem mollitia ipsam sapiente ipsum numquam. Hic maiores, nam eius dolorum odit, ea eveniet possimus porro omnis officia doloremque! Libero?
+            <h2 class="inner-title">Options</h2>
+            <form action="javascript:void(0)" novalidate="novalidate" id="up-settings-optios">
+                <table class="form-table" role="presentation">
+                    <tbody>
+                        <tr>
+                            <th scope="row">
+                                <label for="post-per-page">Forum Per Page:</label>
+                            </th>
+                            <td>
+                                <select name="post_per_page" id="post-per-page" class="regular-text">
+                                    <option value="10">--Select Default--</option>
+                                    <option value="15">15</option>
+                                    <option value="20">20</option>
+                                    <option value="25">25</option>
+                                    <option value="30">30</option>
+                                    <option value="35">35</option>
+                                    <option value="40">40</option>
+                                    <option value="45">45</option>
+                                    <option value="50">50</option>
+                                </select>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p class="submit">
+                    <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+                </p>
+            </form>
         </div>
         <div class="options-author">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, voluptas explicabo! Ab enim voluptatibus reiciendis, totam beatae, facilis est iusto accusantium sapiente eligendi earum maxime sint praesentium consectetur dolorem impedit.
+            <h2 class="inner-title">Author Information</h2>
         </div>
     </div>
 </div>
