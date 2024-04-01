49005c65d27ec1249b9ba5c39272b023987eac9a
rubel <vxlrubel@gmail.com>
2024-04-01 23:11:42 +0600

    update sql query to create table.

--- uni-forum.php
+++ uni-forum.php
@@ -143,14 +143,14 @@
         $table           = $wpdb->prefix . 'uf_likes';
         $charset_collate = $wpdb->get_charset_collate();
 
-        $sql   = "CREATE TABLE IF NOT EXISTS $table(
+        $sql   = "CREATE TABLE IF NOT EXISTS {$table}(
             id INT NOT NULL AUTO_INCREMENT,
             user_id INT NOT NULL,
             post_id INT NOT NULL,
             like_status BOOLEAN DEFAULT FALSE,
             PRIMARY KEY (id),
             UNIQUE KEY unique_user_post (user_id, post_id)
-        ) $charset_collate;";
+        ) {$charset_collate};";
 
         $wpdb->query( $sql );
     }
